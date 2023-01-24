36eb4ddb96c99ac25929500152113f1f19818803
Vassiliki-Marantos <49065746+Vassiliki-Marantos@users.noreply.github.com>
2023-01-24 10:44:24 +0200

    Analysed and updated day 1 code

--- day1/part1and2.py
+++ day1/part1and2.py
@@ -1,12 +1,14 @@
+import heapq
+
 file_path = 'day1\input.txt'
 
-file1 = open(file_path, 'r')
-Lines = file1.readlines()
+with open(file_path, 'r') as f:
+    lines = f.readlines()
 
 calories_per_elf = []
 calories = 0
 
-for line in Lines:
+for line in lines:
     if line.strip() != "":
         calories += int(line.strip())
     else:
@@ -14,15 +16,21 @@ for line in Lines:
         calories = 0
 
 #part 1
+# max functionn - O(n log n)
 max_calories = max(calories_per_elf)
 print("Max calories: ", max_calories)
 
-#part 2
-calories_per_elf.sort()
+#part 2 - using heapq
+#heapq nlargest - O(n log k)  (more efficient than max and sort functions)
 n = 3
+max_three = heapq.nlargest(n, calories_per_elf)
+total_max_three = sum(max_three)
+print("Total of top 3 calories held: ", total_max_three)
 
+#part 2 - using sort
+# sort fucntion - O(n log n)
+calories_per_elf.sort()
 max_three = calories_per_elf[-n:]
 total_max_three = sum(max_three)
-
 print("Total of top 3 calories held: ", total_max_three)
 
